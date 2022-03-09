450e127347a5f3108d2128d21d152c4a833278b6
Shania-F <101061656+Shania-F@users.noreply.github.com>
2022-03-09 21:54:29 +0400

    Update added capacity.py

--- added capacity.py
+++ added capacity.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 from keras.models import load_model
 import cv2
 import numpy as np
@@ -6,24 +5,16 @@ import tkinter
 from tkinter import messagebox
 import smtplib
 
-# Initialize Tkinter
 root = tkinter.Tk()
 root.withdraw()
 
-# Load trained deep learning model
-model = load_model('face_mask_detection_alert_system.h5')
-
-# Classifier to detect face
+model = load_model('face_mask_detection_alert_system.h5') # trained deep learning model
 face_det_classifier=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Capture Video
 vid_source=cv2.VideoCapture(0)
 
-# Dictionaries containing details of Wearing Mask and Color of rectangle around face. If wearing mask then color would be 
-# green and if not wearing mask then color of rectangle around face would be red
-text_dict={0:'Mask ON',1:'No Mask'}
-rect_color_dict={0:(0,255,0),1:(0,0,255)}
-
+text_dict={0:'Mask Present',1:'No Mask'}
+rect_color_dict={0:(0,255,0),1:(0,0,255)} # green for mask, and red for no mask
 SUBJECT = "No Face Mask"   
 TEXT = "Visitor has been detected without a face mask. Entrance is denied."
 
@@ -31,16 +22,14 @@ TEXT = "Visitor has been detected without a face mask. Entrance is denied."
 cap = 1000
 allowedcap=0.5 * cap; #Currently: 50%
 ctr = 0
-
-# While Loop to continuously detect camera feed
+ 
+# continuously detect camera feed
 while(True):
-
     ret, img = vid_source.read()
     grayscale_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_det_classifier.detectMultiScale(grayscale_img,1.3,5)  
 
     for (x,y,w,h) in faces:
-    
         face_img = grayscale_img[y:y+w,x:x+w]
         resized_img = cv2.resize(face_img,(112,112))
         normalized_img = resized_img/255.0
@@ -52,31 +41,32 @@ while(True):
         cv2.rectangle(img,(x,y),(x+w,y+h),rect_color_dict[label],2)
         cv2.rectangle(img,(x,y-40),(x+w,y),rect_color_dict[label],-1)
         cv2.putText(img, text_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,0,0),2) 
-
-        # If label = 1 then it means wearing No Mask and 0 means wearing Mask
+        
+        # label is 1, i.e no mask
         if (label == 1):
-            # Throw a Warning Message to tell user to wear a mask if not wearing one. This will stay
-            #open and No Access will be given He/She wears the mask
+            # display message to tell user to wear a mask 
+            # access is denied till they wear mask
             messagebox.showwarning("Warning","Access Denied. Please wear a Face Mask")
             
-            # Send an email to the administrator if access denied/user not wearing face mask 
+            # email is sent if visitor is not wearing face mask 
             message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
             mail = smtplib.SMTP('smtp.gmail.com', 587)
             mail.ehlo()
-            mail.starttls()
-           # mail.login('test@gmail.com','test')
-           # mail.sendmail('test@gmail.com','test@gmail.com',message)
+            mail.starttls() # tunneled transport layer security
+            mail.login('test@gmail.com','test')
+            mail.sendmail('test@gmail.com','test@gmail.com',message)
             mail.close
         else:
             pass
             break
 
-    cv2.imshow('LIVE Video Feed',img)
+    cv2.imshow('Video Feed',img)
     key=cv2.waitKey(1)
     
+    # key 27 is Esc; video feed ends on pressing Esc
     if(key==27):
         break
-    
+        
 # Capacity Check & Display
 if (label == 0):
     ctr+=1
@@ -86,7 +76,7 @@ if (label == 0):
         print("Current Capacity:",ctr)        
     else:
         print("ENTRY PERMITTED")
-        print("Current Capacity:",ctr)
-
+        print("Current Capacity:",ctr)        
+        
 cv2.destroyAllWindows()
 vid_source.release()
